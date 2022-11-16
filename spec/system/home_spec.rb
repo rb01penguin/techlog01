2d215a5b59e066aa98e5fec761adde3a8889c1e3
rb01penguin <rb01penguin@gmail.com>
2022-11-16 20:52:54 +0900

    Home の System Spec を Headless モードで実行するように変更

--- spec/system/home_spec.rb
+++ spec/system/home_spec.rb
@@ -1,18 +1,23 @@
 require 'rails_helper'
 
 RSpec.describe 'Home', type: :system do
+  # ここから追記
+  before do
+    driven_by :selenium_chrome_headless
+  end
+  # ここまで追記
+
   before do
     driven_by(:rack_test)
   end
 
   # pending "add some scenarios (or delete) #{__FILE__}" ---> 元あったコード念のため
 
-  describe "トップページの検証" do
-    it "Home#top という文字列が表示される" do
+  describe 'トップページの検証' do
+    it 'Home#top という文字列が表示される' do
+      visit '/'
 
-      visit "/"
-
-      expect(page).to have_content("Home#top")
+      expect(page).to have_content('Home#top')
     end
   end
 end
